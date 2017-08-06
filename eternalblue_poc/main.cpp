2ef5560e2acbe810832b21b210c3a1e5e7099325
Zorik Cherfas <zorik.cherfas@gmail.com>
2017-08-06 20:52:57 +0300

    Initial Commit

---
+++ eternalblue_poc/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  eternalblue_poc
+//
+//  Created by Zorik Cherfas on 06/08/2017.
+//  Copyright © 2017 Zorik Cherfas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
