0ca2c99527b1914b8db04ead8da18d907e59bec1
Zorik Cherfas <zorik.cherfas@gmail.com>
2017-08-06 22:24:56 +0300

    Adding smb headers

---
+++ eternalblue_poc/smbHeader.cpp
@@ -0,0 +1,9 @@
+//
+//  smbHeader.cpp
+//  eternalblue_poc
+//
+//  Created by Zorik Cherfas on 06/08/2017.
+//  Copyright © 2017 Zorik Cherfas. All rights reserved.
+//
+
+#include "smbHeader.hpp"
---
+++ eternalblue_poc/smbHeader.hpp
@@ -0,0 +1,14 @@
+//
+//  smbHeader.hpp
+//  eternalblue_poc
+//
+//  Created by Zorik Cherfas on 06/08/2017.
+//  Copyright © 2017 Zorik Cherfas. All rights reserved.
+//
+
+#ifndef smbHeader_hpp
+#define smbHeader_hpp
+
+#include <stdio.h>
+
+#endif /* smbHeader_hpp */
